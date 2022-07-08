49c71d285cce4fda5fb4bf7e8ecaf7230f439e98
leandrochs <leandrochagask@gmail.com>
2022-07-07 21:02:36 -0300

    Updates

---
+++ precache-manifest.8a4891489fe523ef0cf3de5bfef07d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4111dec31728a182687d89201b00a6",
+    "url": "/Starwars-Planets-search/index.html"
+  },
+  {
+    "revision": "8440e51ca8b8a1acca66",
+    "url": "/Starwars-Planets-search/static/css/main.56328471.chunk.css"
+  },
+  {
+    "revision": "06320d236bfdf1958723",
+    "url": "/Starwars-Planets-search/static/js/2.42145407.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Starwars-Planets-search/static/js/2.42145407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8440e51ca8b8a1acca66",
+    "url": "/Starwars-Planets-search/static/js/main.942940b2.chunk.js"
+  },
+  {
+    "revision": "ed94709994bfe4e64e07",
+    "url": "/Starwars-Planets-search/static/js/runtime-main.eb531756.js"
+  }
+]);
